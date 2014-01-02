ae17a7441112558df5548b386d0a97f19cd8960c
Huam Erica <erica@Erica2.local>
2014-01-02 20:43:21 +0100

    msg

--- Banque/creer_compte.sql
+++ Banque/creer_compte.sql
@@ -1,16 +1,16 @@
-CREATE OR REPLACE FUNCTION creer_iban(num_compte public.compte.nbcompte) RETURNS varchar(27) AS $$
+CREATE OR REPLACE FUNCTION creer_iban(num_compte varchar(11)) RETURNS varchar(27) AS $$
 DECLARE
 	iban varchar(27);
 BEGIN
 	SELECT CONCAT( CONCAT( CONCAT( CONCAT(
-					public.banque.code_pays,
-	       	       	       	     	CAST(public.banque.id_banque AS varchar(5))),
-				CAST(public.agence.id_agence AS varchar(5))),
-			public.compte.nb_compte),
-		public.compte.cle_rib)
+					banque.code_pays,
+	       	       	       	     	CAST(banque.id_banque AS varchar(5))),
+				CAST(agence.id_agence AS varchar(5))),
+			compte.nbcompte),
+		compte.cle_rib)
 	INTO iban
-	FROM public.banque NATURAL JOIN (public.agence NATURAL JOIN public.compte)) 
-	WHERE public.compte.nbcompte = num_compte;
+	FROM banque NATURAL JOIN (agence JOIN compte)) 
+	WHERE compte.nbcompte = num_compte;
 	RETURN iban;
 END;
 $$ LANGUAGE plpgsql;
