710283d4fb2ed33e7855d81d0c89495a67d4e66a
Alejandro Castellanos <alejandrocastellanos@MacBook.local>
2014-01-03 10:47:43 +0100

    modele

---
+++ Banque/ins.sql
@@ -0,0 +1,5 @@
+DELETE FROM Personne;
+
+INSERT INTO Personne VALUES (default,'2014888001','Castellanos','Alejandro','12/17/1985','M');
+INSERT INTO Personne VALUES (default,'2014888002','Huam','Erica','08/23/1990','M');
+INSERT INTO Personne VALUES (default,'2014888003','Jamal','Meryam','05/15/1990','M');
