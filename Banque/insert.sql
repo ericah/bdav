3b790f9fabc9be5d6232a6312f554701987718f4
Huam Erica <erica@Erica2.local>
2014-01-05 12:50:07 +0100

    comptes

--- Banque/insert.sql
+++ Banque/insert.sql
@@ -1,4 +1,8 @@
+DELETE FROM Nature_trans;
+DELETE FROM Periodicite;
+DELETE FROM Parametres;
 DELETE FROM Type_carte;
+DELETE FROM Comptes_joints;
 DELETE FROM Compte;
 DELETE FROM Type_compte;
 DELETE FROM Personne;
@@ -8,13 +12,13 @@ DELETE FROM Banque;
 INSERT INTO Banque VALUES (default, 'FirstBank', 'FST BK BIC', 100000);
 INSERT INTO Banque VALUES (default, 'SecondBank', 'SND BK BIC',200000);
 INSERT INTO Banque VALUES (default, 'ThirdBank', 'THRD BK BIC',300000);
-INSERT INTO Agence VALUES (default, 'FirstAgenceFstBK', 001);
-INSERT INTO Agence VALUES (default, 'SndAgenceFstBK', 001);
-INSERT INTO Agence VALUES (default, 'ThirdAgenceFstBK', 001);
-INSERT INTO Agence VALUES (default, 'FirstAgenceSndBK', 002);
-INSERT INTO Agence VALUES (default, 'SecondAgenceSndBK', 002);
-INSERT INTO Personne VALUES (default,'2014888001','Castellanos','Alejandro','12/17/1985','M');
-INSERT INTO Personne VALUES (default,'2014888002','Huam','Erica','08/23/1990','M');
+INSERT INTO Agence VALUES (default, 'FirstAgenceFstBK', 1);
+INSERT INTO Agence VALUES (default, 'SndAgenceFstBK', 1);
+INSERT INTO Agence VALUES (default, 'ThirdAgenceFstBK', 1);
+INSERT INTO Agence VALUES (default, 'FirstAgenceSndBK', 2);
+INSERT INTO Agence VALUES (default, 'SecondAgenceSndBK', 2);
+INSERT INTO Personne VALUES (default,'2014888001','Castellanos','Alejandro','12/17/1985','M',10400);
+INSERT INTO Personne VALUES (default,'2014888002','Huam','Erica','08/23/1990','M',5200);
 INSERT INTO Personne VALUES (default,'2014888003','Jamal','Meryam','05/15/1990','M');
 INSERT INTO Personne VALUES (default,'2014888004','Tonnelier','Jerome','07/23/1988','M');
 INSERT INTO Personne VALUES (default,'2014888005' ,'Maimaiti', 'Amina','06/28/1984','C');
@@ -36,10 +40,29 @@ INSERT INTO nature_trans VALUES (default, 'prelevement');
 INSERT INTO nature_trans VALUES (default, 'cheque');
 INSERT INTO nature_trans VALUES (default, 'retrait');
 
-
+INSERT INTO periodicite VALUES (default, 'Journalier',1);
 INSERT INTO periodicite VALUES (default, 'Hebdomadaire',7);
+INSERT INTO periodicite VALUES (default, 'Bimensuel',15);
 INSERT INTO periodicite VALUES (default, 'Mensuel',30);
+INSERT INTO periodicite VALUES (default, 'Trimestriel',90);
+INSERT INTO periodicite VALUES (default, 'Semestriel',180);
 INSERT INTO periodicite VALUES (default, 'Annuel',360);
 
 INSERT INTO parametres VALUES (default,'entetecompte',2014777000);
 INSERT INTO parametres VALUES (default,'Formule_cheque',201401);
+
+
+INSERT INTO parametres VALUES (default,'bk1codecpt1', 35);
+INSERT INTO parametres VALUES (default,'bk1codecpt2', 15);
+INSERT INTO parametres VALUES (default,'bk1codecpt3', 70);
+INSERT INTO parametres VALUES (default,'bk1codecpt4', 57);
+
+INSERT INTO parametres VALUES (default,'bk2codecpt1', 25);
+INSERT INTO parametres VALUES (default,'bk2codecpt2', 20);
+INSERT INTO parametres VALUES (default,'bk2codecpt3', 75);
+INSERT INTO parametres VALUES (default,'bk2codecpt4', 57);
+
+INSERT INTO parametres VALUES (default,'bk3codecpt1', 30);
+INSERT INTO parametres VALUES (default,'bk3codecpt2', 20);
+INSERT INTO parametres VALUES (default,'bk3codecpt3', 65);
+INSERT INTO parametres VALUES (default,'bk3codecpt4', 55);
