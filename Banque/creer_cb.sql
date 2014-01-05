e94ebe677e6ae4630d51a322f162d15f2badb5f3
Huam Erica <erica@Erica2.local>
2014-01-05 17:06:50 +0100

    cb compte normal

---
+++ Banque/creer_cb.sql
@@ -0,0 +1,62 @@
+
+
+CREATE OR REPLACE FUNCTION creer_cle_sec() RETURNS varchar(3) AS $$
+BEGIN
+	RETURN TRUNC(RANDOM() * 199 + 1);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION random_8char() RETURNS varchar(8) AS $$
+BEGIN
+	RETURN TRUNC(RANDOM() * 199999999 + 1);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION max_date() RETURNS date AS $$
+DECLARE
+	today date;
+BEGIN
+	SELECT date(current_date) INTO today;
+	RETURN (today+ 365*2);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION creer_cb(id_cpt int4, titulaire int4, id_type_cb int4) RETURNS varchar(16) AS $$
+DECLARE
+	bk int4;
+	num varchar(16);
+	nb varchar(11);
+	val varchar(8);
+	titulaire int4;
+	maxi int4;
+	choix_rpc char(1);
+	valide date;
+	cle varchar(3);
+BEGIN
+	SELECT id_banque,NbCompte,ID_titulaire INTO bk,nb,titulaire
+	FROM banque NATURAL JOIN agence NATURAL JOIN compte
+	WHERE id_compte=id_cpt;
+	
+	SELECT valeur::varchar(8) INTO num
+	FROM parametres WHERE nom_para=('formule_cb_bk'||bk);
+
+	SELECT random_8char() INTO val;
+	num := num || val;
+
+	SELECT creer_cle_sec() INTO cle;
+	SELECT max_date() INTO valide;
+
+	IF id_type_cb=1 OR id_type_cb=4 THEN choix_rpc = 'R';
+	ELSIF id_type_cb=2 OR id_type_cb=5 THEN choix_rpc = 'P';
+	ELSIF id_type_cb=3 OR id_type_cb=6 THEN choix_rpc = 'C';
+	ELSE RAISE NOTICE 'Type CB unknown';
+	END IF;
+
+	SELECT valeur INTO maxi
+	FROM parametres WHERE nom_para=('cb'||id_type_cb||'bk'||bk||'plafond');
+
+	INSERT INTO carte_bancaire VALUES (num,cle,valide,titulaire,choix_rpc,maxi,id_type_cb,nb);
+	
+	RETURN num;
+END;
+$$ LANGUAGE plpgsql;
